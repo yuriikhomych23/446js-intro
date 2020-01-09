5fdd90938b6d19e92f7904a669352bc289db1368
yuriikhomych23 <yuriikhomych94@gmail.com>
2020-01-09 16:45:50 +0000

    Sample code

--- js/index.js
+++ js/index.js
@@ -1,24 +1,25 @@
 const greetings = 'Hello JavaScript';
 alert(greetings);
 
-let userName; // ми створили пусту змінну, undefined
+let userName;
 userName = prompt('Enter your name');
 alert('Hello ' + userName);
-const userAge = parseFloat(prompt ('Enter your age'));
+
+const userAge = parseFloat(prompt('Entry your age'));
 alert('Your age is ' + userAge);
 
-const nextAge = addOne(userAge);
+const nextAge = addOne(userAge); //відноситься до функції
 alert('Next year you will be ' + nextAge);
 
-if (Number.isNaN (userAge)) {
-    alert ('Wrong age!');
+if (Number.isNaN(userAge)) {
+    alert ('Wrong age');
 } else if (userAge >= 18) {
-    alert ('You are adult');
+    alert('You are adult');
 } else {
-    alert ('You are too young');
+    alert('You are too young');
 }
 
-function addOne(input){
+function addOne(input) {
     const result = input + 1;
     return result;
 }
